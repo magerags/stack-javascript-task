a9ffc07ed3d39810e878b7c9af48ec965b2bfdd8
Tom Whittle <tomwhittle@hey.com>
2021-04-23 12:06:56 +0200

    Finished exercises

--- stack.js
+++ stack.js
@@ -1,1 +1,34 @@
 // Your implementation goes here
+class Stack {
+  constructor(...elements) {
+    this.elements = elements;
+  }
+
+  get size() {
+    return this.elements.length;
+  }
+
+  push(element) {
+    return this.elements.push(element);
+  }
+
+  pop() {
+    if (this.size == 0) return null;
+    return this.elements.pop();
+  }
+
+  peek() {
+    return this.elements[this.size - 1];
+  }
+
+  swap() {
+    const a = this.elements;
+    [a[this.size - 2], a[this.size - 1]] = [a[this.size - 1],a[this.size - 2]];
+  }
+
+  isEmpty() {
+    return this.elements.length == 0;
+  }
+}
+
+module.exports = Stack;
